3158dc06d738d15f784268d323224be1397b7b7c
Rihards Edmunds Cerps <goldminelv@gmail.com>
2021-09-15 10:04:04 +0300

    Created basic frontend

--- frontend/bootcampbankingapp/src/main.js
+++ frontend/bootcampbankingapp/src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import axios from 'axios';
+import axios from 'axios'
 import router from './router'
 
 const app = createApp(App).use(router);
