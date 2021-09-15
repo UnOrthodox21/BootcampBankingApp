06bd8b0c82fdcbee796f7e5df9c292808e298d02
Janis <janis.odins92@gmail.com>
2021-09-15 16:00:02 +0300

    created SettingsForm

---
+++ frontend/bootcampbankingapp/src/components/settings/SettingsForm.vue
@@ -0,0 +1,59 @@
+<template>
+<div>
+    <p>Settings test</p>
+</div>
+
+<div class="container">
+    <div class="row">
+        <div class="col-12">
+<form>
+  <div class="form-group my-5"  style="width:300px;margin:auto;">
+    <label for="emailInput">Email address </label>
+    <input type="email" class="form-control "    id="emailInput" placeholder="name@example.com" >
+  </div>
+ 
+   <div class="form-group my-5" style="width:300px;margin:auto;">
+       <!-- ja grib atsevisku margin bottom
+    <label class="mb-5" for="exampleFormControlInput2">Name </label> -->
+    <label for="nameInput">Name </label>
+    <input type="text" class="form-control "    id="nameInput" >
+  </div>
+
+  <div class="form-group my-5" style="width:300px;margin:auto;">
+    <label for="surnameInput">Surname </label>
+    <input type="text" class="form-control "    id="surnameInput" >
+  </div>
+
+  <div class="form-group my-5" style="width:300px;margin:auto;">
+    <label for="phoneInput">Phone number </label>
+    <input type="number" class="form-control "    id="phoneInput" >
+  </div>
+
+  <div class="form-group my-5" style="width:300px;margin:auto;">
+    <label for="addressInput">Phone number </label>
+    <input type="text" class="form-control "    id="addressInput" >
+  </div>
+   <button class="btn btn-primary" type="submit">Submit form</button>
+</form>
+        </div>
+    </div>
+ </div>
+
+
+</template>
+
+<script>
+export default {
+    name: "Settings Form",
+    components: {
+
+    },
+    props: ["user"]
+}
+</script>
+
+<style scoped>
+    label {
+        margin-bottom:0;
+    }
+</style>
