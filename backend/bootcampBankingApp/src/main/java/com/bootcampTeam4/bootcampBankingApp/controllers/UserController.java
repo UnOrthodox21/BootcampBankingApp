705f1c8c462c36fc5b088e75a5b2ccada670b2e6
Rihards Edmunds Cerps <goldminelv@gmail.com>
2021-09-19 14:59:13 +0300

    Merged back in User elements

---
+++ backend/bootcampBankingApp/src/main/java/com/bootcampTeam4/bootcampBankingApp/controllers/UserController.java
@@ -0,0 +1,65 @@
+package com.bootcampTeam4.bootcampBankingApp.controllers;
+
+
+import com.bootcampTeam4.bootcampBankingApp.models.User;
+import com.bootcampTeam4.bootcampBankingApp.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    private UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userService.getAllUsers();
+    }
+
+    @GetMapping(path = "{userId}")
+    public Optional<User> getUserById(@PathVariable("userId") Long userId) {
+        return userService.getUserById(userId);
+    }
+
+    @PostMapping
+    public void addNewUser(@RequestBody User user) {
+        userService.addNewUser(user);
+    }
+
+    @DeleteMapping(path = "{userId}")
+    public void deleteUser(@PathVariable("userId") Long userId) {
+        userService.deleteUser(userId);
+    }
+
+    @PutMapping(path = "{userId}")
+    public void updateUser(
+            @PathVariable("userId") Long userId,
+            @RequestParam(required = false) String userName,
+            @RequestParam(required = false) String email,
+            @RequestParam(required = false) String firstName,
+            @RequestParam(required = false) String lastName,
+            @RequestParam(required = false) String address,
+            @RequestParam(required = false) int phone,
+            @RequestParam(required = false) String password) {
+        userService.updateUser(userId, userName, email, firstName, lastName, address, phone, password);
+    }
+}
+
+
+
+
+
+
+
+
+
+
---
+++ backend/bootcampBankingApp/src/main/java/com/bootcampTeam4/bootcampBankingApp/models/User.java
@@ -0,0 +1,133 @@
+package com.bootcampTeam4.bootcampBankingApp.models;
+
+import org.springframework.security.core.GrantedAuthority;
+
+import javax.persistence.*;
+import java.util.Collection;
+
+    @Entity
+    @Table(name = "users")
+    public class User {
+
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private long id;
+
+        @Column(name = "user_name")
+        private String username;
+
+        @Column(name = "email")
+        private String email;
+
+        @Column(name = "first_name")
+        private String firstName;
+        //
+        @Column(name = "last_name")
+        private String lastName;
+
+        @Column(name = "address")
+        private String address;
+
+        @Column(name = "phone")
+        private int phone;
+
+        @Column(name = "password")
+        private String password;
+
+        public User() {}
+
+        public User(long id, String username, String email, String firstName, String lastName, String address, int phone, String password) {
+            this.id = id;
+            this.username = username;
+            this.email = email;
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.address = address;
+            this.phone = phone;
+            this.password = password;
+        }
+
+        public long getId() {
+            return id;
+        }
+
+        public void setId(long id) {
+            this.id = id;
+        }
+
+        public String getUsername() {
+            return username;
+        }
+
+
+        public void setUsername(String username) {
+            this.username = username;
+        }
+
+        public String getEmail() {
+            return email;
+        }
+
+        public void setEmail(String email) {
+            this.email = email;
+        }
+
+        public String getFirstName() {
+            return firstName;
+        }
+
+        public void setFirstName(String firstName) {
+            this.firstName = firstName;
+        }
+
+        public String getLastName() {
+            return lastName;
+        }
+
+        public void setLastName(String lastName) {
+            this.lastName = lastName;
+        }
+
+        public String getAddress() {
+            return address;
+        }
+
+        public void setAddress(String address) {
+            this.address = address;
+        }
+
+        public int getPhone() {
+            return phone;
+        }
+
+        public void setPhone(int phone) {
+            this.phone = phone;
+        }
+
+        public String getPassword() {
+            return password;
+        }
+
+        public void setPassword(String password) {
+            this.password = password;
+        }
+
+
+
+        @Override
+        public String toString() {
+            return "User{" +
+                    "id=" + id +
+                    ", userName='" + username + '\'' +
+                    ", email='" + email + '\'' +
+                    ", firstName='" + firstName + '\'' +
+                    ", lastName='" + lastName + '\'' +
+                    ", address='" + address + '\'' +
+                    ", phone=" + phone +
+                    ", password='" + password +
+                    '}';
+        }
+
+
+    }
+
