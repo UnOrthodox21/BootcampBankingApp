84488d29aa5951cfcc73c02d4afe4a6ff6471c2f
VPTaurus <mazurspavels@gmail.com>
2021-09-21 14:10:47 +0300

    Popup + tablex2

--- frontend/bootcampbankingapp/src/components/bank-accounts/BankAccountsTable.vue
+++ frontend/bootcampbankingapp/src/components/bank-accounts/BankAccountsTable.vue
@@ -1,11 +1,11 @@
 <template>
-<button type="button" class="btn btn-top-menu">Add savings account</button>
+
 <button type="button" class="btn btn-top-menu">Add new adress</button>
 <button type="button" class="btn btn-top-menu">Show transaction history</button>
-<button type="button" class="btn btn-top-menu">Send money</button>
+<router-link class="btn btn-top-menu" to="/transactions">Send money</router-link>
 
 
-   <table class="table" >
+  <table class="table" >
   <thead>
     <tr>
       <th scope="col">Account adress</th>
@@ -15,27 +15,14 @@
     </tr>
   </thead>
   <tbody>
-    <tr>
-      <th scope="row">{{bankAccounts.number}}</th>
-      <td>{{bankAccounts.type}}</td>
-      <td>{{bankAccounts.balance}}</td>
-      <td><button type="button" class="btn btn-outline-success">Edit</button>
-      <button type="button" class="btn btn-outline-danger">Remove</button></td>
+    <tr v-bind:for="(bankAccount,index) in bankAccounts"> 
+      <th scope="row">{{ bankAccount.number }}</th>
+      <td>{{ bankAccount.type }}</td>
+      <td>{{ bankAccount.balance }}</td>
+      <td><button type="button" class="btn btn-outline-success" @click="() => TogglePopup('buttonTrgger')">Edit</button>
+      <button type="button" class="btn btn-outline-danger" @click="deleteBankAccount(bankAccount.id)" >Remove</button></td>
     </tr>
-    <tr>
-     <th scope="row">{{bankAccounts.number}}</th>
-      <td>{{bankAccounts.type}}</td>
-      <td>{{bankAccounts.balance}}</td>
-      <td><button type="button" class="btn btn-outline-success">Edit</button>
-      <button type="button" class="btn btn-outline-danger">Remove</button></td>
-    </tr>
-    <tr>
-     <th scope="row">{{bankAccounts.number}}</th>
-      <td>{{bankAccounts.type}}</td>
-      <td>{{bankAccounts.balance}}</td>
-      <td><button type="button" class="btn btn-outline-success">Edit</button>
-      <button type="button" class="btn btn-outline-danger">Remove</button></td>
-    </tr>
+    
   </tbody>
 </table>
 </template>
@@ -46,7 +33,19 @@ export default {
     components: {
       
     },
-    props: ["bankAccounts"]
+    props: ["user","bankAccounts"],
+     methods: {
+      
+    
+      deleteBankAccount(id){
+        if(window.confirm("Are You sure?")){
+          this.$http.delete(process.env.VUE_APP_API_URL + "/bank-accounts/" + id)
+          .then(() => this.$parent.$parent.setUser())
+          .catch(err => console.log(err));
+        }
+      }
+     }
+
 }
 </script>
 
