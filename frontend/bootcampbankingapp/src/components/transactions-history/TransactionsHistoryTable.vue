1706ae526a7cc2931883635c624d2cd59d4aad1d
Janis <janis.odins92@gmail.com>
2021-09-22 08:27:14 +0300

    transactionhistory started

---
+++ frontend/bootcampbankingapp/src/components/transactions-history/TransactionsHistoryTable.vue
@@ -0,0 +1,18 @@
+<template>
+    <div> text </div>
+</template>
+
+<script>
+
+
+export default {
+  name: "TransactionsHistoryTable",
+
+  props: ["transactions"]
+}
+</script>
+
+<style scoped>
+
+</style>
+
